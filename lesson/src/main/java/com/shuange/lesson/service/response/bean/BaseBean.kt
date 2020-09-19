b3515fe975c2bfdddaa7ac0c66ba4485eb5a378c
dougisadog <wzc2542736@gmail.com>
2020-09-19 11:37:11 +0800

    [update] update the response beans

---
+++ lesson/src/main/java/com/shuange/lesson/service/response/bean/BaseBean.kt
@@ -0,0 +1,9 @@
+package com.shuange.lesson.service.response.bean
+
+open class BaseBean {
+    val id: Int = 0
+    var createdBy: String = ""
+    var createdDate: String = ""
+    var lastModifiedBy: String = ""
+    var lastModifiedDate: String = ""
+}
